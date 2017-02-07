651d2415b62072876e51e9e25336971242c37a69
Luciano Fiandesio <luciano@fiandes.io>
2017-02-07 20:22:04 +0100

    Reusable Components

--- src/App.js
+++ src/App.js
@@ -100,12 +100,9 @@ class Table extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
               <span>
-                <button
-                  onClick={() => onDismiss(item.objectID)}
-                  type="button"
-                >
+                <Button onClick={() => onDismiss(item.objectID)}>
                 Dismiss
-                </button>
+                </Button>
               </span>
             </div> 
         )}
@@ -114,6 +111,30 @@ class Table extends Component {
     );
 
   }
+
+}
+
+class Button extends Component {
+
+  render() {
+    const {
+      onClick,
+      className = '',
+      children
+    } = this.props;
+
+    return (
+      <button 
+        onClick={onClick}
+        className={className}
+        type='button'
+      >
+      {children}
+      </button>
+    );
+
+  }
+
 
 }
 
