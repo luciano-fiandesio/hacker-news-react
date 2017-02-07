448405206337be8a439eadf7369f59434365ed3e
Luciano Fiandesio <luciano@fiandes.io>
2017-02-07 20:09:40 +0100

    Breakup Your Components

--- src/App.js
+++ src/App.js
@@ -56,13 +56,41 @@ class App extends Component {
     const {searchTerm, list} = this.state
     return (  
       <div className="App">
-        <form>
+        <Search 
+            value={searchTerm}
+            onChange={this.onSearchChange}/>
+        <Table
+            list={list}
+            pattern={searchTerm}
+            onDismiss={this.onDismiss}/>
+       </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const {value, onChange} = this.props;
+    return (
+      <form>
           <input type="text" 
-                 onChange={this.onSearchChange}
-                 value={searchTerm}
+                 onChange={onChange}
+                 value={value}
                  />
-        </form>
-        { list.filter(isSearched(searchTerm)).map(item => 
+        </form>  
+
+    );
+  }
+
+}
+
+class Table extends Component {
+
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item => 
             
             <div key={item.objectID}> 
               <span><a href={item.url}>{item.title}</a></span>
@@ -71,7 +99,7 @@ class App extends Component {
               <span>{item.points}</span>
               <span>
                 <button
-                  onClick={() => this.onDismiss(item.objectID)}
+                  onClick={() => onDismiss(item.objectID)}
                   type="button"
                 >
                 Dismiss
@@ -79,9 +107,12 @@ class App extends Component {
               </span>
             </div> 
         )}
-       </div>
+
+      </div>
     );
+
   }
+
 }
 
 export default App;
