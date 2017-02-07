478cce6164512a3b35f17f6af442c06281587788
Luciano Fiandesio <luciano@fiandes.io>
2017-02-07 20:01:48 +0100

    ES6 Destructuring

--- src/App.js
+++ src/App.js
@@ -53,15 +53,15 @@ class App extends Component {
   }
 
   render() {
-    
-    return (
+    const {searchTerm, list} = this.state
+    return (  
       <div className="App">
         <form>
           <input type="text" 
                  onChange={this.onSearchChange}
                  />
         </form>
-        { this.state.list.filter(isSearched(this.state.searchTerm)).map(item => 
+        { list.filter(isSearched(searchTerm)).map(item => 
             
             <div key={item.objectID}> 
               <span><a href={item.url}>{item.title}</a></span>
