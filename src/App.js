611625e8d1c7209db32444113ee11590a715de7d
Luciano Fiandesio <luciano@fiandes.io>
2017-02-07 19:08:57 +0100

    ES6 Arrow Functions

--- src/App.js
+++ src/App.js
@@ -25,8 +25,7 @@ class App extends Component {
     
     return (
       <div className="App">
-        { list.map(function(item) {
-          return (
+        { list.map(item => 
             
             <div key={item.objectID}> 
               <span><a href={item.url}>{item.title}</a></span>
@@ -34,8 +33,7 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
             </div> 
-          );
-        })}
+        )}
        </div>
     );
   }
